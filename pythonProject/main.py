3aea0d478533e98de7eac23472dc679dfd230b6a
Your Name <you@example.com>
2022-02-23 14:13:37 +0100

    c

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,54 +1,95 @@
 import os
 import sys
+import math
+import multiprocessing as mp
+
 import matplotlib.pyplot as plt
-
-path_notebook = '/home/yashuai/Software/adehome/workspace/commonroad-search/tutorials/1_search_algorithms'
-# add the SMP folder to python path
-sys.path.append(os.path.join(path_notebook, "../../"))
-
-# add the 1_search_algorithms folder to python path
-sys.path.append(os.path.join(path_notebook, "../"))
-
 from commonroad.common.file_reader import CommonRoadFileReader
-#from commonroad.visualization.draw_dispatch_cr import draw_object
 from commonroad.visualization.mp_renderer import MPRenderer
-
 from SMP.motion_planner.motion_planner import MotionPlanner
 from SMP.maneuver_automaton.maneuver_automaton import ManeuverAutomaton
-from SMP.motion_planner.utility import plot_primitives, display_steps
+from utils import visualize_solution, display_steps
 
-# load scenario
-path_scenario = os.path.join(path_notebook, "../../scenarios/tutorial/")
-id_scenario = 'ZAM_Tutorial_Urban-3_2'
+from autoware_auto_planning_msgs.msg import TrajectoryPoint
+from autoware_auto_planning_msgs.msg import Trajectory
+from builtin_interfaces.msg import Duration
+from commonroad.geometry.shape import Rectangle
+from commonroad.scenario.obstacle import StaticObstacle, ObstacleType
+from commonroad.scenario.trajectory import State
+
+
+def angle_to_heading(angle):
+    real = math.cos(angle * 0.5)
+    imag = math.sin(angle * 0.5)
+    return real, imag
+
+
+def f1(scenario, planning_problem, planner_DFS):
+    # prepare input for visualization
+    scenario_data = (scenario, planner_DFS.state_initial, planner_DFS.shape_ego, planning_problem)
+
+    # display search steps
+    p1 = mp.Process(target=display_steps, args=(scenario_data, planner_DFS.execute_search, planner_DFS.config_plot))
+    p1.start()
+
+
+def f2(path):
+    traj = create_trajectory_from_list_states(path)
+    p2 = mp.Process(target=visualize_solution, args=(scenario, planning_problem_set, traj))
+    p2.start()
+
 
 # read in scenario and planning problem set
-scenario, planning_problem_set = CommonRoadFileReader("/home/yashuai/Software/adehome/workspace/dfg-car/output/1.xml").open()
+scenario, planning_problem_set = CommonRoadFileReader("/home/drivingsim/workspace/dfg-car/output/1.xml").open()
 # retrieve the first planning problem in the problem set
 planning_problem = list(planning_problem_set.planning_problem_dict.values())[0]
 
-# plot the scenario and the planning problem set
-plt.figure(figsize=(15, 5))
-rnd = MPRenderer()
-scenario.draw(rnd, draw_params={'lanelet': {"show_label": True}})
-planning_problem_set.draw(rnd)
-rnd.render()
-
 # load the xml with stores the motion primitives
-name_file_motion_primitives = 'V_0.0_20.0_Vstep_2.0_SA_-1.066_1.066_SAstep_0.18_T_0.5_Model_BMW_320i.xml'
+name_file_motion_primitives = 'V_9.0_9.0_Vstep_0_SA_-0.2_0.2_SAstep_0.4_T_0.5_Model_BMW320i.xml'
 # generate automaton
 automaton = ManeuverAutomaton.generate_automaton(name_file_motion_primitives)
 
 
 # construct motion planner
-planner_DFS = MotionPlanner.DepthFirstSearch(scenario=scenario,
-                                             planning_problem=planning_problem,
-                                             automaton=automaton)
-# prepare input for visualization
-scenario_data = (scenario, planner_DFS.state_initial, planner_DFS.shape_ego, planning_problem)
+planner_DFS = MotionPlanner.BreadthFirstSearch(scenario=scenario,
+                                               planning_problem=planning_problem,
+                                               automaton=automaton)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+f1(scenario, planning_problem, planner_DFS)
 
-# display search steps
-display_steps(scenario_data=scenario_data,
-              algorithm=planner_DFS.execute_search,
-              config=planner_DFS.config_plot)
-print('####################')
+from SMP.motion_planner.utility import create_trajectory_from_list_states
+path, _, _ = planner_DFS.execute_search()
+if path is not None:
+    f2(path)
+
+positions = []
+traj = Trajectory()
+traj.header.frame_id = "map"
+if path is not None:
+    for primitive in path:
+        for state in primitive:
+            point = TrajectoryPoint()
+            point.time_from_start = Duration(sec=state.time_step)
+            point.x = state.position[0]
+            point.y = state.position[1]
+            point.z = 0.0
+            real, imag = angle_to_heading(state.orientation)
+            point.heading.real = real
+            point.heading.imag = imag
+            point.longitudinal_velocity_mps = state.velocity
+            # point.heading_rate_rps = state.yaw_rate
+
+            traj.points.append(point)
+            positions.append(state.position)
+
+rnd = MPRenderer()
+for position in positions:
+    ego_vehicle_id = scenario.generate_object_id()
+    ego_vehicle_type = ObstacleType.CAR
+    ego_vehicle_shape = Rectangle(width=2, length=3)
+    ego_vehicle_state = State(position=position, orientation=0)
+    static = StaticObstacle(ego_vehicle_id, ego_vehicle_type, ego_vehicle_shape, ego_vehicle_state)
+    scenario.add_objects(static)
+scenario.draw(rnd, draw_params={'lanelet': {"show_label": False}})
+rnd.render()
 plt.show()
---
+++ pythonProject/utils.py
@@ -0,0 +1,107 @@
+import enum
+from typing import List
+
+import matplotlib.pyplot as plt
+from commonroad.geometry.shape import Rectangle
+from commonroad.planning.planning_problem import PlanningProblemSet
+from commonroad.prediction.prediction import TrajectoryPrediction
+from commonroad.scenario.obstacle import ObstacleType, DynamicObstacle
+from commonroad.scenario.scenario import Scenario
+from commonroad.scenario.trajectory import State, Trajectory
+# import CommonRoad-io modules
+from commonroad.visualization.mp_renderer import MPRenderer
+
+
+@enum.unique
+class MotionPrimitiveStatus(enum.Enum):
+    IN_FRONTIER = 0
+    INVALID = 1
+    CURRENTLY_EXPLORED = 2
+    EXPLORED = 3
+    SOLUTION = 4
+
+
+def plot_primitive_path(mp: List[State], status: MotionPrimitiveStatus, plotting_params):
+    plt.plot(mp[-1].position[0], mp[-1].position[1], color=plotting_params[status.value][0], marker='o', markersize=8,
+             zorder=27)
+    x = []
+    y = []
+    for state in mp:
+        x.append(state.position[0])
+        y.append(state.position[1])
+    plt.plot(x, y, color=plotting_params[status.value][0], marker="", linestyle=plotting_params[status.value][1],
+             linewidth=plotting_params[status.value][2], zorder=25)
+
+
+def display_steps(scenario_data, algorithm, config, **args):
+    scenario = scenario_data[0]
+    initial_state = scenario_data[1]
+    ego_shape = scenario_data[2]
+    planning_problem = scenario_data[3]
+
+    plt.figure(figsize=(10, 10))
+    plt.axis('equal')
+    renderer = MPRenderer()
+    draw_params = {'scenario': {'lanelet': {'facecolor': '#F8F8F8'}}}
+    scenario.draw(renderer, draw_params=draw_params)
+
+    ego_vehicle = DynamicObstacle(obstacle_id=scenario.generate_object_id(), obstacle_type=ObstacleType.CAR,
+                                  obstacle_shape=ego_shape,
+                                  initial_state=initial_state)
+    ego_vehicle.draw(renderer)
+    planning_problem.draw(renderer)
+
+    renderer.render()
+
+    if 'limit_depth' in args:
+        path, primitives, list_states_nodes = algorithm(limit_depth=args['limit_depth'])
+    else:
+        path, primitives, list_states_nodes = algorithm()
+
+    for node_status in list_states_nodes:
+        for node in node_status.values():
+            plot_primitive_path(node[0], node[1], config.PLOTTING_PARAMS)
+            plt.pause(0.1)
+    plt.show()
+
+
+def visualize_solution(scenario: Scenario,
+                       planning_problem_set: PlanningProblemSet,
+                       trajectory: Trajectory) -> None:
+    num_time_steps = len(trajectory.state_list)
+
+    # create the ego vehicle prediction using the trajectory and the shape of the obstacle
+    dynamic_obstacle_initial_state = trajectory.state_list[0]
+    dynamic_obstacle_shape = Rectangle(width=1.8, length=4.3)
+    dynamic_obstacle_prediction = TrajectoryPrediction(trajectory, dynamic_obstacle_shape)
+
+    # generate the dynamic obstacle according to the specification
+    dynamic_obstacle_id = scenario.generate_object_id()
+    dynamic_obstacle_type = ObstacleType.CAR
+    dynamic_obstacle = DynamicObstacle(dynamic_obstacle_id,
+                                       dynamic_obstacle_type,
+                                       dynamic_obstacle_shape,
+                                       dynamic_obstacle_initial_state,
+                                       dynamic_obstacle_prediction)
+
+    # visualize scenario
+    plt.figure(figsize=(10, 10))
+    renderer = MPRenderer()
+    for i in range(0, num_time_steps):
+        renderer.clear()
+        scenario.draw(renderer, draw_params={'time_begin': i})
+        planning_problem_set.draw(renderer)
+        dynamic_obstacle.draw(renderer, draw_params={'time_begin': i,
+                                                     'dynamic_obstacle': {'shape': {'facecolor': 'green'},
+                                                                          'trajectory': {'draw_trajectory': True,
+                                                                                         'facecolor': '#ff00ff',
+                                                                                         'draw_continuous': True,
+                                                                                         'z_order': 60,
+                                                                                         'line_width': 5}
+                                                                          }
+                                                     })
+
+        plt.gca().set_aspect('equal')
+        renderer.render()
+        plt.pause(0.1)
+    plt.show()
