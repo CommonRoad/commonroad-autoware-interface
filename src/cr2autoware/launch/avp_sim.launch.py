67b9a2b18252d70951bbb106174a5844d0afc657
Koray Koca <koray.koca@tum.de>
2022-05-29 21:37:32 +0200

    use avp_core.launch inside cr2autoware

--- src/cr2autoware/launch/avp_sim.launch.py
+++ src/cr2autoware/launch/avp_sim.launch.py
@@ -14,8 +14,7 @@
 
 """Launch Modules for Milestone 3 of the AVP 2020 Demo."""
 
-# from ament_index_python import get_package_share_directory
-from ament_index_python import *
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
