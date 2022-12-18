d9981c249d46bbc5753ae71d3d64a68df9901356
Florian Weiser <florian.weiser@tum.de>
2022-12-18 14:54:06 +0100

    Update cr2autoware install cript

--- cr2autoware_install.sh
+++ cr2autoware_install.sh
@@ -4,6 +4,7 @@
 # place this file in the autoware folder
 
 echo "Starting Cr2Autoware Setup"
+sudo apt-get update
 sudo apt install -y python3-pykdl
 cd src/universe/autoware.universe/planning/tum_commonroad_planning/commonroad-scenario-designer
 pip install -r requirements.txt
@@ -12,6 +13,7 @@ pip install -r requirements.txt
 cd ../reactive_planner
 pip install -r requirements.txt
 pip install methodtools
+pip install utm
 pip install commonroad-io==2022.1
 pip install commonroad-drivability-checker==2021.4
 pip install commonroad-vehicle-models==2.0.0
