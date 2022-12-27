503c59851bfbdb57225e49d02a51a515963bea19
Florian Weiser <florian.weiser@tum.de>
2022-12-27 12:47:55 +0100

    Update commonroad dependency library versions

--- cr2autoware_install.sh
+++ cr2autoware_install.sh
@@ -13,9 +13,10 @@ cd ../reactive_planner
 pip install -r requirements.txt
 pip install methodtools
 pip install utm
-pip install commonroad-io==2022.1
-pip install commonroad-drivability-checker==2021.4
+pip install commonroad-io==2022.3
+pip install commonroad-drivability-checker==2022.2.1
 pip install commonroad-vehicle-models==2.0.0
-pip install commonroad-route-planner==2022.1
+pip install commonroad-route-planner==2022.3
 pip install shapely==1.8.5
+pip install -U numpy
 echo "Cr2Autoware Setup completed!"
