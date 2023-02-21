9484a7c5f967eadd2d52caf82cf877b3004e6205
Gerald Würsching <gerald.wuersching@tum.de>
2023-02-21 14:30:10 +0100

    Fix and Update setup.py

--- src/cr2autoware/setup.py
+++ src/cr2autoware/setup.py
@@ -4,8 +4,16 @@ from glob import glob
 package_name = 'cr2autoware'
 
 setup(
+    # package info
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
+    zip_safe=True,
+    author='Cyber-Physical Systems Group, Technical University of Munich',
+    author_email='commonroad@lists.lrz.de',
+    description='Interface between CommonRoad and Autoware.Universe',
+    license='TODO: License declaration',
+
+    # Source
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,26 +23,31 @@ setup(
         ('share/' + package_name + '/config', glob('config/*')),
         ('share/' + package_name + '/param', glob('param/*.param.yaml')),
     ],
+
+    # Requirements
+    python_requires='>=3.7',
     install_requires=[
     	'setuptools',
-    	'commonroad-io>=2022.3',
-    	'commonroad-drivability-checker>=2021.4',
-    	'commonroad-vehicle-models>=2.0.0',
-    	'commonroad-route-planner>=2022.1',
+    	'commonroad-io==2022.3',
+    	'commonroad-drivability-checker==2022.2.1',
+    	'commonroad-vehicle-models>=3.0.0',
+    	'commonroad-route-planner==2022.3',
     	'pyproj',
     	'pyyaml',
     	'utm'
     	],
-    zip_safe=True,
-    maintainer='drivingsim',
-    maintainer_email='drivingsim@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'cr2autoware_node = cr2autoware.cr2autoware:main',
         ],
     },
+
+    # Additional Information
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
 
