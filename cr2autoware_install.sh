b4288b3a03dc66ba6b0b532afb7688df80d5b612
Gerald Wuersching <gerald.wuersching@tum.de>
2023-02-27 19:16:09 +0100

    update setup bash script

--- cr2autoware_install.sh
+++ cr2autoware_install.sh
@@ -1,22 +1,49 @@
 #! /bin/sh
 
-# This is an automatic setup of cr2autoware when used in the TUM Autoware launch
+# This is setup script of cr2autoware to automatically fetch submodules and install pip dependencies
 
-echo "Starting Cr2Autoware Setup"
-sudo apt-get update
+# Constants
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+function print_progress() {
+  print "${GREEN}${1}${NC}" "${2}"
+}
+
+print_progress "<CR2Autoware>: Starting Cr2Autoware Setup" -n
+
+# initialize and update submodules
+# TODO: remove if dependencies are fetched via vcs
+print_progress "<CR2Autoware>: Fetching submodules..." -n
+git submodule init
+git submdule update
+print_progress "Done" -n
+
+# install apt packages
+print_progress "<CR2Autoware>: Installing apt packages..." -n
+# TODO: include apt dependencies in dockerfile?
+sudo apt-get update -y
 sudo apt install -y python3-pykdl
+# TODO Bugfix: missing in Dockerimage
+sudo apt-get install -y liblttng-ust-dev
+print_progress "Done" -n
+
+# install pip packages
+print_progress "<CR2Autoware>: Installing pip packages..." -n
+
+cd dfg-car
+pip install -r src/cr2autoware/requirements.txt
+
 cd ../commonroad-scenario-designer
-pip install -r requirements.txt
+pip install .
+
 cd ../commonroad-search
-pip install -r requirements.txt
+pip install .
+
 cd ../reactive_planner
-pip install -r requirements.txt
-pip install methodtools
-pip install utm
-pip install commonroad-io==2022.3
-pip install commonroad-drivability-checker==2022.2.1
-pip install commonroad-vehicle-models==3.0.0
-pip install commonroad-route-planner==2022.3
-pip install shapely==1.8.5
+pip install .
+
+# TODO: Bugfix numpy version
 pip install -U numpy
-echo "Cr2Autoware Setup completed!"
+
+print_progress "<CR2Autoware>: Cr2Autoware Setup completed!" -n
