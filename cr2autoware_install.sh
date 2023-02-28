144cddcf0ddb346278d527bd1f9b0193d0e64ee8
Gerald Wuersching <gerald.wuersching@tum.de>
2023-02-28 15:15:50 +0100

    setup bash script

--- cr2autoware_install.sh
+++ cr2autoware_install.sh
@@ -6,6 +6,15 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+
+function print() {
+  echo -e "${1}"
+  if [ "${2}" == "-n" ]; then
+    echo -e ""
+  fi
+}
+
+
 function print_progress() {
   print "${GREEN}${1}${NC}" "${2}"
 }
@@ -16,7 +25,7 @@ print_progress "<CR2Autoware>: Starting Cr2Autoware Setup" -n
 # TODO: remove if dependencies are fetched via vcs
 print_progress "<CR2Autoware>: Fetching submodules..." -n
 git submodule init
-git submdule update
+git submodule update
 print_progress "Done" -n
 
 # install apt packages
@@ -32,16 +41,26 @@ print_progress "Done" -n
 print_progress "<CR2Autoware>: Installing pip packages..." -n
 
 cd dfg-car
+print_progress "<CR2Autoware>: Installing dfg-car..." -n
 pip install -r src/cr2autoware/requirements.txt
+print_progress "Done" -n
 
 cd ../commonroad-scenario-designer
+print_progress "<CR2Autoware>: Installing commonroad-scenario-designer..." -n
 pip install .
+print_progress "Done" -n
 
 cd ../commonroad-search
+print_progress "<CR2Autoware>: Installing commonroad-search..." -n
 pip install .
+print_progress "Done" -n
 
-cd ../reactive_planner
+cd ../reactive-planner
+print_progress "<CR2Autoware>: Installing reactive-planner..." -n
 pip install .
+print_progress "Done" -n
+
+cd ..
 
 # TODO: Bugfix numpy version
 pip install -U numpy
