b048322a86d6dcc786530044f952575e29b12bfa
Gerald Würsching <gerald.wuersching@tum.de>
2023-06-27 16:38:31 +0200

    Load yaml params

--- src/cr2autoware/cr2autoware/cr2autoware.py
+++ src/cr2autoware/cr2autoware/cr2autoware.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 import os
 import traceback
 from typing import Optional
+import yaml
 
 # Autoware message imports
 from autoware_auto_planning_msgs.msg import Trajectory as AWTrajectory  # type: ignore
@@ -56,7 +57,6 @@ from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
-import yaml
 
 from cr2autoware.rp_interface import RP2Interface
 from cr2autoware.scenario_handler import ScenarioHandler
@@ -81,7 +81,8 @@ class Cr2Auto(Node):
         super().__init__(node_name="cr2autoware")  # type: ignore
 
         # Declare ros parameters
-        param = yaml.load(open(os.path.dirname(__file__) + "/ros_param.yaml"))
+        with open(os.path.dirname(__file__) + "/ros_param.yaml", 'r') as stream:
+            param = yaml.load(stream, Loader=yaml.Loader)
         for key, value in param.items():
             self.declare_parameter(key, value)
 
