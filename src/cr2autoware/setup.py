4322304d636f9f6186745ef306a4c15399e0c9a8
Gerald Wuersching <gerald.wuersching@tum.de>
2024-06-24 14:19:51 +0200

    version numbers

--- src/cr2autoware/setup.py
+++ src/cr2autoware/setup.py
@@ -6,12 +6,12 @@ package_name = 'cr2autoware'
 setup(
     # package info
     name=package_name,
-    version='0.0.1',
+    version='0.1',
     zip_safe=True,
     author='Cyber-Physical Systems Group, Technical University of Munich',
     author_email='commonroad@lists.lrz.de',
     description='Interface between CommonRoad and Autoware.Universe',
-    license='TODO: License declaration',
+    license='BSD',
 
     # Source
     packages=find_packages(),
@@ -25,16 +25,16 @@ setup(
     ],
 
     # Requirements
-    python_requires='>=3.7',
+    python_requires='>=3.10',
     install_requires=[
-    	'setuptools',
-    	'commonroad-io>=2022.3',
-    	'commonroad-drivability-checker==2023.1',
-    	'commonroad-vehicle-models>=3.0.0',
-    	'commonroad-route-planner==2022.3',
-    	'pyproj',
-    	'pyyaml',
-    	'utm'
+    	'setuptools==69.0.2',
+    	'commonroad-io==2024.1',
+    	'commonroad-drivability-checker==2024.1',
+    	'commonroad-vehicle-models==3.0.2',
+    	'commonroad-route-planner==2024.2.0',
+    	'pyproj>=3.4.1',
+    	'pyyaml==6.0',
+    	'utm>=0.7.0'
     	],
     tests_require=['pytest'],
     entry_points={
@@ -45,13 +45,10 @@ setup(
 
     # Additional Information
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
         "Operating System :: POSIX :: Linux",
     ],
     # include *yaml configs in packaging
     include_package_data=True,
     package_data={'': ['*.yaml']}
-
 )
-
